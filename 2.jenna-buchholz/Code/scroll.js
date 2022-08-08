d6404e7bab2fbb87a564f5183d5825ab7ceac636
Adan <adanaligakhar@gmail.com>
2022-08-08 19:21:23 +0500

    added transition and auto-color

--- 2.jenna-buchholz/Code/scroll.js
+++ 2.jenna-buchholz/Code/scroll.js
@@ -4,6 +4,7 @@ const progressTag = document.querySelector('div.progress');
 const sections = document.querySelectorAll('section');
 const clientTag = document.querySelector('div.client');
 const pageTag = document.querySelector('div.page');
+const headerTag = document.querySelector('header')
 
 //when we scroll the page update the pixel tag to how far we scrolled
 
@@ -38,9 +39,19 @@ document.addEventListener('scroll',function () {
  
 
   sections.forEach(section => {
-    if(section.offsetTop <= pixels){
+    if(section.offsetTop - 100 <= pixels){
       clientTag.innerHTML = section.getAttribute('data-client')
       pageTag.innerHTML =  section.getAttribute('data-page')
+
+      if(section.hasAttribute('data-is-dark')){
+        headerTag.classList.add('white');
+        progressTag.classList.add('white')
+        
+      }else{
+        headerTag.classList.remove('white')
+        progressTag.classList.remove('white')
+
+      }
 
     }
   })
