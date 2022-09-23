e3ceff07e5d9e2c6185aecf6a88f89e0be76bcce
Adan <adanaligakhar@gmail.com>
2022-09-23 12:15:41 +0500

    header animation

---
+++ 3.Brux/animation.js
@@ -0,0 +1,40 @@
+const animatedTags = document.querySelectorAll("h2, h3, p, section img, a.button");
+
+//fade out on load
+
+animatedTags.forEach(tag => {
+    tag.style.opacity = 0;
+});
+
+const fadeIn = function() {
+
+    let delay = 0.25;
+
+    animatedTags.forEach(tag => {
+        const tagTop = tag.getBoundingClientRect().top;
+        const tagBottom = tag.getBoundingClientRect().bottom;
+        if(tagTop < window.innerHeight && tagBottom > 0){
+       
+            tag.style.animation  = `fadein 1s ${delay}s both`;
+            delay = delay + 0.25
+
+        } else {
+            tag.style.opacity = 0;
+            tag.style.animation = '';
+        }
+    })
+
+
+}
+
+fadeIn();
+
+document.addEventListener('scroll', () => {
+    fadeIn();
+});
+
+//on browser resize
+
+window.addEventListener('resize', () => {
+    fadeIn();
+})
